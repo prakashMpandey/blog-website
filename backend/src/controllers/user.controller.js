f96fc96342e1b4bc9c6e06bbe2f340bddd251cd4
prakash <prakashmanipadndey685@gmail.com>
2024-04-27 14:11:22 +0530

    user controller finished

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -170,7 +170,115 @@ const logOutUser=async(req,res)=>{
     
 }
 
-export {registerUser,logOutUser,logInUser}
+const updatePassword=asyncHandler(async(req,res)=>{
+    //check login
+    
+    //get old password,new password
+    //check if available
+    //compare old pasword
+    //update new password
+
+  
+     try {
+        const {oldPassword,newPassword}=req.body
+        console.log(oldPassword,newPassword)
+       
+        if(!(oldPassword&&newPassword))
+        {
+            throw new ApiError(400,"both fields required")
+        }
+        
+        const user=await User.findById(req.user?._id)
+        console.log(user)
+        
+        const ispasswordRight=await user.isPasswordCorrect(oldPassword)
+       if(!ispasswordRight)
+       {
+        throw new ApiError(400,"wrong password entered")
+       }
+    //    console.log(ispasswordRight)
+    if(oldPassword===newPassword)
+    {
+        res.json(new ApiResponse(400,{},"new password must be different"))
+    }
+       
+        user.password = newPassword;
+        // console.log(user.password)
+        await user.save({validateBeforeSave: false})
+      
+    
+        return res.status(200).json(new ApiResponse(200,{},"password changer successfully"))
+    
+    
+      
+      
+   
+   }
+     catch (error) {
+        res.status(500).send(error)
+        
+     }
+    }
+)
+
+const updateDetails=asyncHandler(async(req,res)=>{
+    try {
+        const {email,username,fullname}=req.body
+        console.log(email)
+        if(!(email||username||fullname))
+        {
+            res.json(new ApiResponse(400,"no field specified"))
+    
+        }
+        const user=await User.findByIdAndUpdate(req.user._id,{$set:{fullname,email:email,username}},{new:true,password:0,refreshToken:0})
+    
+    
+        console.log(user.email,user.fullname);
+    
+        return res.status(200).json(new ApiResponse(200,user,"details changed successfully"))
+    } 
+    
+    catch (error) {
+        res.status(500).json(500,{error},"something wrong while changing details")
+        
+    }
+})
+const updateAvatar=asyncHandler(async(req,res)=>{
+    
+    const avatarLocalPath = req.file?.path
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar file is missing")
+    }
+
+    //TODO: delete old image - assignment
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    console.log(avatar)
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading on avatar")
+        
+    }
+
+
+    const user = await User.findByIdAndUpdate(
+        req.user?._id,
+        {
+            $set:{
+                avatar: avatar.url
+            }
+        },
+        {new: true}
+    ).select("-password")
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, user, "Avatar image updated successfully")
+    )
+})
+
+export {registerUser,logOutUser,logInUser,updatePassword,updateDetails,updateAvatar}
 
 
 
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser,logInUser,logOutUser } from "../controllers/user.controller.js";
+import { registerUser,logInUser,logOutUser,updatePassword,updateDetails ,updateAvatar} from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/Auth.middleware.js";
 
@@ -10,11 +10,14 @@ router.route("/register").post(upload.single("avatar"),registerUser
   
 )
 router.route("/login").post(logInUser)
-
+4786
 
 //secured routes
 
 router.route("/logout").post(verifyJWT,logOutUser)
+router.route("/changePassword").post(verifyJWT,updatePassword)
+router.route("/updateDetails").post(verifyJWT,updateDetails)
+router.route("/updateAvatar").post(verifyJWT,upload.single("avatar"),updateAvatar)
 
 
 
