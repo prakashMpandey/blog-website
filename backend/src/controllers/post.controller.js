d6b520febd20deb26d51a735be12fe27d2891fbd
prakash <prakashmanipadndey685@gmail.com>
2024-04-28 22:23:14 +0530

    written getallpost function

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -6,11 +6,6 @@ import { deleteOnCloudinary, uploadOnCloudinary } from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 const createPost = asyncHandler(async (req, res) => {
-  //get title and content
-  //get image
-  //check if exist//
-  //get user id
-  //push on database
   const { title, content } = req.body;
 
   if (!(title && content)) {
@@ -28,78 +23,116 @@ const createPost = asyncHandler(async (req, res) => {
     postImage: postImage?.url || "",
     author: req.user,
   });
-  if(!post)
-  {
-    res.json(new ApiResponse(400,post,"post not uploaded"))
-
+  if (!post) {
+    res.json(new ApiResponse(400, post, "post not uploaded"));
   }
-return res.status(200).json(new ApiResponse(200,post,"post uploaded successfully"))
-
+  return res
+    .status(200)
+    .json(new ApiResponse(200, post, "post uploaded successfully"));
 });
-const getPost=asyncHandler(async (req,res)=>{
-  const postId=req.params.postId
+const getPost = asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
 
-  if(!postId)
-  {
-    console.log("post id not found")
+  if (!postId) {
+    console.log("post id not found");
   }
-  const post=await Post.findById(postId)
-  console.log(post)
-  res.status(200).json(new ApiResponse(200,post,"post found successfully"))
+  const post = await Post.findById(postId);
+  console.log(post);
+  res.status(200).json(new ApiResponse(200, post, "post found successfully"));
+});
+const deletePost = asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
 
-})
-const deletePost=asyncHandler(async (req,res)=>{
-  const postId=req.params.postId
-  
-  if(!postId)
-  {
-    console.log("post id not found")
+  if (!postId) {
+    console.log("post id not found");
   }
-  const userId=String(req.user._id)
-  const post=await Post.findById(postId)
-  const author=String(post.author._id)
- 
-  if(userId!=author)
-  {
-    console.log("you cannot delete post")
-    console.log(userId,author)
-  }
-  
-  await Post.findByIdAndDelete(postId)
+  const userId = String(req.user._id);
+  const post = await Post.findById(postId);
+  const author = String(post.author._id);
 
-  res.send(200,{},"post deleted successfully")
-
-})
-const updatePost=asyncHandler(async(req,res)=>{
-  const postId=req.params.postId
-  
-  const {title,content}=req.body
-
-  if(!postId)
-  {
-    console.log("post id not found")
+  if (userId != author) {
+    console.log("you cannot delete post");
+    console.log(userId, author);
   }
 
+  await Post.findByIdAndDelete(postId);
+
+  res.send(200, {}, "post deleted successfully");
+});
+const updatePost = asyncHandler(async (req, res) => {
+  const postId = req.params.postId;
+
+  const { title, content } = req.body;
+
+  if (!postId) {
+    console.log("post id not found");
+  }
+
+  if (!(title || content)) {
+    console.log("no field found");
+  }
+
+  const updatedPost = await Post.findByIdAndUpdate(
+    postId,
+    { $set: { title: title, content: content } },
+    { new: 0 }
+  );
+
+  if (!updatedPost) {
+    console.log("some problem while changing");
+  }
+  console.log(updatedPost);
+  res.status(200).json(200, updatedPost, "post updated successfully");
+});
+const getAllPost = asyncHandler(async (req, res) => {
+ try {
+   let { query, sortBy, sortType, page = 1, limit = 10 } = req.query;
+   page = Number(page);
+   limit = Number(limit);
+   let sort = 1;
+   let sortByField = "title";
    
-  if(!(title || content))
-  {
-    console.log("no field found")
-  }
+   if (!query) {
+     console.log("Search value not found");
+   }
+ 
 
-  const updatedPost=await Post.findByIdAndUpdate(postId,{$set:{title:title,content:content}},{new:0})
+ 
+   if (sortType === "desc") {
+     sort = -1;
+   }
+ 
+   if (sortBy) {
+     sortByField = sortBy;
+   }
+ 
+   console.log(sortByField, sort);
+   const skip=(page - 1) * limit
+   const post = await Post.aggregate([
+     {
+       $match: {
+         title: { $regex: query, $options: "i" },
+       },
+     },
+     {
+       $sort: {
+         [sortByField]: sort,
+       },
+     },
+     {
+       $skip: skip, 
+     },
+     {
+       $limit: limit, 
+     },
+   ]);
+ 
+   console.log(post);
+   res.status(200).json(new ApiResponse(200,post,"search result found"))
+ } 
+ catch (error) {
+  res.status(500).json(new ApiResponse(500,error,"something went wrong while searching document"))
+ }
+});
 
-  if(!updatedPost)
-  {
-    console.log("some problem while changing")
-  }
-  console.log(updatedPost)
-   res.status(200).json(200,updatedPost,"post updated successfully")
-
-
-})
-const getAllPost=asyncHandler(async (req,res)=>{
-  const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query
-  console.log(req.query)
-})
-
-export {createPost,getPost,deletePost,updatePost,getAllPost}
+export { createPost, getPost, deletePost, updatePost, getAllPost };
