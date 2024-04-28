c4fa012a1588082f5e89e6b2cd6ad112d3798a28
prakash <prakashmanipadndey685@gmail.com>
2024-04-28 14:04:03 +0530

    post routes created

--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { upload } from "../middleware/multer.middleware.js";
 import { verifyJWT } from "../middleware/Auth.middleware.js";
-import { createPost } from "../controllers/video.controller.js";
+import { createPost, getPost,deletePost,updatePost, getAllPost } from "../controllers/post.controller.js";
 
 const router=Router()
 
 router.route("/createPost").post(verifyJWT,upload.single("postImage"),createPost)
+router.route("/getPost/:postId").post(verifyJWT,getPost)
+router.route("/deletePost/:postId").post(verifyJWT,deletePost)
+router.route("/updatePost/:postId").post(verifyJWT,updatePost)
+router.route("/getAllPost").post(verifyJWT,getAllPost)
 
 export default router
